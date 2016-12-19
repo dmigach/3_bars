ab4f4416d01954e653216846087ffe8c0e5e741f
Dmitry Gachkovsky <sacethi0@gmail.com>
2016-12-19 17:51:01 +0300

    parameterized duplicating code
    closest bar function improved

--- bars.py
+++ bars.py
@@ -1,7 +1,6 @@
 import json
 import codecs
 import os
-from operator import itemgetter
 
 
 def load_data(file_path):
@@ -13,35 +12,34 @@ def load_data(file_path):
 
 
 def get_biggest_bar(bars_list):
-    return [max(bars_list,
-                key=itemgetter('SeatsCount'))[x] for x in ['Name', 'Address']]
+    return [max(bars_list, key=lambda x:
+            x['SeatsCount'])[x] for x in ['Name', 'Address']]
 
 
 def get_smallest_bar(bars_list):
-    return [min(bars_list,
-                key=itemgetter('SeatsCount'))[x] for x in ['Name', 'Address']]
+    return [min(bars_list, key=lambda x:
+            x['SeatsCount'])[x] for x in ['Name', 'Address']]
 
 
 def get_closest_bar(bars_list, usr_longitude, usr_latitude):
-    estimated_distances = []
-    for bar in bars_list:
-        estimated_distances.append(abs(float(bar['Latitude_WGS84']) -
-                                       usr_latitude) +
-                                   abs(float(bar['Longitude_WGS84']) -
-                                       usr_longitude))
-    return [bars_list[estimated_distances.index(min(estimated_distances))][x]
+    return [min(bars_list, key=lambda x:
+            abs(float(x['Latitude_WGS84']) - usr_latitude) +
+            abs(float(x['Longitude_WGS84']) - usr_longitude))[x]
             for x in ['Name', 'Address']]
 
+
+def get_user_coordinate(coordinate_name):
+    try:
+        return float(input('Enter your'
+                           ' current {}\n'.format(coordinate_name.lower())))
+    except ValueError:
+        raise SystemExit('{} must be a number'.format(coordinate_name))
+
+
 if __name__ == '__main__':
-    list_of_bars = load_data(input('Enter file path' + '\n'))
-    try:
-        user_longitude = float(input('Enter your current longitude\n'))
-    except ValueError:
-        raise SystemExit('Longitude must be a number')
-    try:
-        user_latitude = float(input('Enter your current latitude\n'))
-    except ValueError:
-        raise SystemExit('Latitude must be a number')
+    list_of_bars = load_data(input('Enter file path\n'))
+    user_longitude = get_user_coordinate('Longitude')
+    user_latitude = get_user_coordinate('Latitude')
     print('The biggest bar is {},'
           ' located at {}'.format(*get_biggest_bar(list_of_bars)))
     print('The smallest bar is {},'
